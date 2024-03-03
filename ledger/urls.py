fe1a21c11f3278b4810366f6c7bfb95dc41c7b3e
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-03-04 00:56:44 +0800

    updated ledger urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import recipe_list, list_1, list_2
+from .views import recipe_list, recipe_detail
 
 urlpatterns = [
     path('recipes/list', recipe_list, name='list'),
-    path('recipe/1', list_1, name='1'),
-    path('recipe/2', list_2, name='2'),
+    path('recipe/<int:id>/ingredients', recipe_detail, name='recipe-detail')
 ]
 
 app_name = "ledger"
