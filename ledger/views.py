e0e64a956a29babaaa936a781b322b5cd7d42b20
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-03-04 20:44:04 +0800

    updated views.py

--- ledger/views.py
+++ ledger/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from .models import Recipe, Ingredient, RecipeIngredient
+from .models import Recipe, RecipeIngredient
 
 # Create your views here.
 
